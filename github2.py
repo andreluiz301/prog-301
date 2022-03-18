41192ae4d7fe81f66d9041d397e85a914d99015e
André <andrelbarcelo@gmail.com>
2022-03-18 17:04:27 -0300

    Teste (18/03/2022 17:04:27)

--- github2.py
+++ github2.py
@@ -31,7 +31,7 @@ time.sleep(6)
 py.write(f'start {nav} https://github.com/')
 time.sleep(0.5)
 py.press('enter')
-time.sleep(10)
+time.sleep(20)
 for a in range(a):
     py.press('tab')
 time.sleep(0.5)
