b79c919a087bd682cf17e26e5001d782b6a9954c
André <andrelbarcelo@gmail.com>
2022-03-18 16:04:15 -0300

    18/03/2022

--- README.md
+++
@@ -1,2 +0,0 @@
-# prog-301
-Repositório para aulas de programação - 301 Info - 2022.
---
+++ github.py
@@ -0,0 +1,16 @@
+import pyautogui as py
+import time, os
+
+
+time.sleep(1)
+py.hotkey('ctrl', 'alt', 't')
+time.sleep(5)
+py.write('cd prog-301')
+time.sleep(0.5)
+py.press('enter')
+time.sleep(0.5)
+py.write('git add *')
+time.sleep(0.5)
+py.press('enter')
+time.sleep(1)
+py.write(f'git commit -m {time.strftime(""" "%d/%m/%Y" """)}')
