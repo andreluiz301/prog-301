ff850987960d917983ebf22f31bd42c8601e0c56
Andre <andrelbarcelo@gmail.com>
2022-03-29 16:59:00 -0300

    Teste (29/03/2022 16:58:58)

---
+++ teste.py
@@ -0,0 +1,9 @@
+while True:
+    e = input('Está erecido? y/n -> ')
+
+    if e == 'y':
+        print('SECHO')
+    elif e == 'n':
+        print('Broxa')
+    else:
+        print('Rei da Barriga Branca')
