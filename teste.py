a0e237ae55592c41ed11be8acd4716727219b925
Andre <andrelbarcelo@gmail.com>
2022-04-01 15:52:07 -0300

    Teste (01/04/2022 15:52:05)

--- teste.py
+++ teste.py
@@ -1,9 +1,29 @@
-a = input('Você gostaria de calcular? s/n -> ')
+import sqlite3
 
-if a == 's':
-    num = input('Digite a conta: ')
-    print(eval(num))
-elif a == 'n':
-    print('Boboca')
-else:
-    print('OTARO')
+
+class BancoDao(object):
+
+    def __init__(self) -> None:
+        self.con = None
+
+    def conectar(self):
+        if self.con is None:
+            self.con = sqlite3.connect('pessoa.db')
+            self.cur = self.con.cursor()
+
+    def desconectar(self):
+        if self.con is not None:
+            self.con.close()
+            self.con = None
+
+    def criar_tabela(self):
+        self.conectar()
+        self.cur.execute("""
+                            CREATE TABLE IF NOT EXISTS pessoa(
+                                idPessoa integer PRIMARY KEY AUTOINCREMENT,
+                                nome text NOT NULL,
+                                idade integer NOT NULL
+                            );
+        """)
+        self.con.commit()
+        self.desconectar()
